a001ca1f3aba9a09633fbf008626056312c66ddb
rusty015 <nailrusty.dev@gmail.com>
2024-09-24 21:45:55 +0900

    update upload image

--- index.ts
+++ index.ts
@@ -1,5 +1,16 @@
-import { uploadToIPFS } from "./utils";
+import { uploadMetadata, uploadToIPFS } from "./utils";
 
 (async function main() {
     const imageHash = await uploadToIPFS("assets/1.png");
+    const imageUri = `https://gateway.pinata.cloud/ipfs/${imageHash}`;
+    console.log("imageURI ", imageUri)
+    
+    const metadataHash = await uploadMetadata({
+        name: "ColCore",
+        description: "This is a core NFT made by Rusty",
+        image: imageUri,
+    });
+    
+    const metadataUri = `https://gateway.pinata.cloud/ipfs/${metadataHash}`;
+    console.log("matadataURI ", metadataUri);
 })();
--- utils/index.ts
+++ utils/index.ts
@@ -5,6 +5,7 @@ import { createSignerFromKeypair, keypairIdentity } from '@metaplex-foundation/u
 import bs58 from "bs58";
 import fs from "fs";
 import axios from "axios";
+import FormData from "form-data";
 import { PINATA_APIKEY, PAYER_PRIVATEKEY, CLUSTERS, TX_CONFIG, PINATA_FILE_URL, PINATA_ORIGIN_URL } from '../config';
 import { METADATA } from '../types';
 
@@ -16,20 +17,21 @@ const payerSigner = createSignerFromKeypair(umi, payer);
 umi.use(keypairIdentity(payerSigner));
 
 export const uploadToIPFS = async (filePath: string) => {
-    const data = new FormData();
-    data.append("file", fs.createReadStream(filePath));
     try {
-         const res = await axios.post(`https://api.pinata.cloud/pinning/pinFileToIPFS`, data, {
+        const data = new FormData();
+
+        data.append("file", fs.createReadStream(filePath));
+
+        const res = await axios.post(PINATA_FILE_URL, data, {
             maxContentLength: Infinity,
             headers: {
-                "Content-Type": "multipart/form-data",
+                "Content-Type": `multipart/form-data; boundary=${data.getBoundary()}`,
                 'Authorization': `Bearer ${PINATA_APIKEY}`
             },
         });
-        console.log(res.data.IpfsHash)
+        return res.data.IpfsHash;
     } catch (error) {
         console.error('Failed to send request:', error);
-        throw error;
     }
 };
 
